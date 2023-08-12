e63d3da9c0345a373d7475f8035658d3d28384c7
wuhaibin <wuhaibin@utry.com>
2023-08-12 17:00:50 +0800

    自动部署

---
+++ assets/chunks/virtual_search-data.2d6a5085.js
@@ -0,0 +1,1 @@
+const t={map:'[{"测试":["1.0"]},{},{},{},{},{},{},{},{}]'},e={"1.0":{t:"测试",p:"",l:"knowledge/2023/08/12.html",a:"测试"}},o={previewLength:100,buttonLabel:"搜索",placeholder:"请输入关键词",allow:[],ignore:[]},a={INDEX_DATA:t,PREVIEW_LOOKUP:e,Options:o};export{a as default};
